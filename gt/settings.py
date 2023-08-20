abc8fdf54c90021eb6d8c93e564a078b173f2c72
Manav Mistry <137400266+Manav541@users.noreply.github.com>
2023-08-20 14:48:35 +0530

    Update settings.py

--- gt/settings.py
+++ gt/settings.py
@@ -142,7 +142,7 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'manavmistry05@gmail.com'
-EMAIL_HOST_PASSWORD = 'Mr__mnv541'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 CART_SESSION_ID = 'cart'
